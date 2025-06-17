dd1d1402b7fca9c33e716cae0c0f1fb17b7be821
Garg-Sarthak <garg.sarthak9527@gmail.com>
2025-06-17 21:56:31 +0530

    added kafka dockerfile, user activity is now logged to kafka client

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ const uuid = require("uuid");
 const express = require('express');
 const { createClient } = require('redis');
 const {WebSocketServer} = require("ws")
+const {Kafka} = require('kafkajs')
 
 const {partyRooms} = require("./globalVars")
+
 
 const app = express();
 app.use(express.json());
@@ -30,6 +32,36 @@ async function connectRedis() {
     }
 }
 connectRedis()
+
+const kafka = new Kafka({
+    clientId : "watch-party-app",
+    brokers : ['localhost:9092']
+})
+const producer = kafka.producer();
+
+async function connectKafka(){
+    try{
+        await producer.connect();
+        console.log('kafka connected')
+    }catch(e){ console.log(e, 'kafka connection error') }
+}
+connectKafka()
+
+
+async function sendKafkaEvent(topic, payload) {
+  try {
+    // await producer.connect()
+    await producer.send({
+      topic: topic,
+      messages: [{ value: JSON.stringify(payload) }],
+    });
+    console.debug("kafka message sent successfully to topic", topic);
+  } catch (error) {
+    console.error(`Failed to send event to Kafka topic ${topic}:`, error);
+  }
+}
+
+
 
 
 
@@ -68,6 +100,13 @@ app.post('/party',async (req,res)=> {
         "timestamp": 0             
     }
     
+    sendKafkaEvent('party-event',{
+        "timestamp" : time.toISOString(),
+        "partyId" : partyId,
+        "userId" : req.body.hostId,
+        "mediaId" : req.body.mediaId,
+        "eventType" : "create-party"
+    })
     
     const serverHost = process.env.HOST;
     // console.log(serverHost)
@@ -110,6 +149,7 @@ app.get('/party/:partyId',async(req,res) => {
     res.send(JSON.parse(partyVal))
 })
 
+
 const server = app.listen(8080, () => {
     console.log(`Example app listening on port 8080`)
     console.log(partyRooms.get("test"));
@@ -124,32 +164,42 @@ const wss = new WebSocketServer({ server });
 
 wss.on('connection', function connection(ws,req) {
     
+    // gets params from url
+    const params = new URLSearchParams(req.url.split("?")[1]);
+    const partyId = params.get("partyId");
+    const userId = params.get("userId")
 
-    const partyId = req.url.split("?")[1].split("&")[0].split("=")[1];
-    // const params = new URLSearchParams(req.url.slice(req.url.indexOf('?')));
-    // const partyId = params.get("partyId");
-
-    if (!partyId) {
-        console.log("Connection rejected: No partyId provided.");
+    // missing params reject connection
+    if (!partyId || !userId) {
+        console.log("Connection rejected: No partyId or userId provided.");
         ws.close();
         return;
     }
 
-    // console.log(partyId);
 
     // adds ws connection to dedicated party room
     ws.partyId = partyId;
+    ws.userId = userId;
     if (!partyRooms.get(partyId)) {
         partyRooms.set(partyId,new Set());
     }
     partyRooms.get(partyId).add(ws);
+
+    sendKafkaEvent('user-events',{
+        "timestamp" : new Date().toISOString(),
+        "userId" : userId,
+        "partyId" : partyId,
+        "eventType" : "join-party"
+    })
+    
 
     // check for addition of ws to party room
     console.log(`party ${partyId} now has ${partyRooms.get(partyId).size} members`);
     
     // receives message -> sends to pub-sub
     ws.on('message', function message(data) {
-        console.log('received: %s', data);
+        // console.log('received: %s', data);
+
         try{
             data = JSON.parse(data.toString());
         }catch(e){
@@ -158,9 +208,20 @@ wss.on('connection', function connection(ws,req) {
             return;
         }
 
+
         if (data.type == "controls" || data.type == "chat"){
             const channel = `party-${data.type}:${partyId}`;
             client.publish(channel,JSON.stringify(data))
+            sendKafkaEvent('engagement-events',{
+            "timestamp" : new Date().toISOString(),
+            "userId" : userId,
+            "partyId" : partyId,
+            "eventType" : "message-sent",
+            "messageType" : data.type,
+            "messageContent" : data.type=='controls'?data.message:"chat messages are not stored to protect user privacy"
+        })
+        }else{
+            console.log("invalid message type : must be 'controls' or 'chat'");
         }
 
         
@@ -175,6 +236,12 @@ wss.on('connection', function connection(ws,req) {
         if (room){
             room.delete(ws);
             console.log(`party ${partyId} now has ${partyRooms.get(partyId).size} members`);
+            sendKafkaEvent('user-events',{
+                "timestamp" : new Date().toISOString(),
+                "userId" : userId,
+                "partyId" : partyId,
+                "eventType" : "leave-party"
+            })
         }
     });
 
@@ -186,7 +253,6 @@ wss.on('connection', function connection(ws,req) {
 
 subscriber.pSubscribe(['party-controls:*','party-chat:*'],(message,channel) => {
     const partyId = channel.split(":")[1];
-    // const type = channel.split(":")[0].split("-")[1];
     const room = partyRooms.get(partyId)
     if (room){
         for (const client of room){
@@ -194,7 +260,3 @@ subscriber.pSubscribe(['party-controls:*','party-chat:*'],(message,channel) => {
         }
     }
 })
-
-module.exports = {
-    server
-}
